2c8b55e33d50623276f760dea2c0604aa47feb26
蒲公英小子 <865648178@qq.com>
2024-09-17 00:15:45 +0800

    咳咳咳

--- scripts/check.ts
+++ scripts/check.ts
@@ -1,4 +1,4 @@
-import subscription from '../src/subscription';
+import subscription from '../src/subscription1';
 import { checkSubscription } from '@gkd-kit/tools';
 
 checkSubscription(subscription);
